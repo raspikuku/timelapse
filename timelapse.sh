77b3a7159ab8b8b72a519cd067279756900609e4
RaspiKuKu <raspikuku@users.noreply.github.com>
2016-03-11 09:47:28 -0500

    Add raspistill parameters

--- timelapse.sh
+++ timelapse.sh
@@ -1,9 +1,10 @@
+#!/bin/bash
 ROLL=$(cat /home/pi/timelapse/series)
 SAVEDIR=/home/pi/timelapse
 while [ true ]; do
-filename=$ROLL-$(date -u +"%d%m%Y_%H%M-%S").jpg
-/opt/vc/bin/raspistill -n -o $SAVEDIR/$filename
+  filename=$ROLL-$(date -u +"%d%m%Y_%H%M-%S").jpg
+  /opt/vc/bin/raspistill -n -t 1000 -w 1024 -h 768 -o $SAVEDIR/$filename
 
-sleep 4;
+  sleep 9;
 
 done;
