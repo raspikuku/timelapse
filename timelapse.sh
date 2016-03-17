5b1298f118059a2521fb1733b6d70086fcf9acca
RaspiKuKu <raspikuku@users.noreply.github.com>
2016-03-17 11:31:22 -0500

    Create a new dir for every roll

--- timelapse.sh
+++ timelapse.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 ROLL=$(cat /home/pi/timelapse/series)
-SAVEDIR=/home/pi/timelapse
+SAVEDIR=/home/pi/timelapse/$ROLL
+
+mkdir -p $SAVEDIR
+i=1
+
 while [ true ]; do
-  filename=$ROLL-$(date -u +"%d%m%Y_%H%M-%S").jpg
+  filename=$(printf image_%04d.jpg ${i%.*})
+
   /opt/vc/bin/raspistill -n -t 1000 -w 1024 -h 768 -o $SAVEDIR/$filename
 
   sleep 9;
+  
+  (( i ++ ))
 
 done;
